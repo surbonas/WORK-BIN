c1f623f1038b2eae167718f3e8681f8b723cecb0
VLN-SURBO-6540\stu <stanislovas.urbonas@markmonitor.com>
2016-09-20 14:45:56 +0300

    Some useless features.

--- GitSandboxProject/GitSandboxProject/Program.cs
+++ GitSandboxProject/GitSandboxProject/Program.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SandboxShared;
 
 namespace GitSandboxProject
 {
     class Program
     {
         private static SandboxClass sandbox;
+        private static SharedClassExtended extendedSharedClass;
 
         static void Main(string[] args)
         {
             sandbox = new SandboxClass();
-            sandbox.DoSomething("param");
+            sandbox.DoSomething("another param");
+
+            extendedSharedClass = new SharedClassExtended();
+            sandbox.DoSomething(string.Empty);
         }
 
     }
--- GitSandboxProject/SandboxShared/SharedClassExtended.cs
+++ GitSandboxProject/SandboxShared/SharedClassExtended.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SandboxShared
 {
-    public class SharedClass
+    public class SharedClassExtended
     {
     }
 }
