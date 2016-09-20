b66e1a559fdbad4589fc3f71f3331ac127e3295c
VLN-SURBO-6540\stu <stanislovas.urbonas@markmonitor.com>
2016-09-20 14:47:04 +0300

    Added BugFixClass

--- GitSandboxProject/GitSandboxProject/Program.cs
+++ GitSandboxProject/GitSandboxProject/Program.cs
@@ -14,6 +14,8 @@ namespace GitSandboxProject
         {
             sandbox = new SandboxClass();
             sandbox.DoSomething("param");
+
+            BugFixClass.BugFixMethod();
         }
 
     }
